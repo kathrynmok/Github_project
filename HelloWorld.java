e0279003cf18df50c0faf7c3fd1aaabddc3fbeb6
Kathryn Mokhov <kathryn.mokhov@mrjaffesclass.com>
2022-09-27 11:43:27 -0700

    workflow

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class programflow here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        if
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
